bfe57e4fcf01e420272e5004d3909ca509b62d36
Gabriel Gomes <glga@gft.com>
2018-08-21 20:11:15 -0300

    (pagamento) Desenvolvimento da Tela - Refatoração

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
@@ -2,16 +2,35 @@ package br.com.alura.aluraviagens.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.moedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
 
+        setTitle(TITULO_APPBAR);
 
+        Pacote pacote = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal(243.99));
+
+        mostraPreco(pacote);
+
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBrasileira = moedaUtil.formataMoedaBrasileira(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
