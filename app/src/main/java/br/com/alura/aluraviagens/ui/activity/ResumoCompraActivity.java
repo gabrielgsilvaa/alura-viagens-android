4dd06f2b9a0009a8fb8d9b7318ac3f956f3840e3
Gabriel Gomes <glga@gft.com>
2018-08-21 20:56:30 -0300

    (resumo da compra) Desenvolvimento da Tela - Refatoração

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -29,20 +29,33 @@ public class ResumoCompraActivity extends AppCompatActivity {
 
         Pacote pacote = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal(243.99));
 
-        TextView local = findViewById(R.id.resumo_compra_local_pacote);
-        local.setText(pacote.getLocal());
+        mostraLocal(pacote);
+        mostraImagem(pacote);
+        mostraData(pacote);
+        mostraPreco(pacote);
 
-        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
-        Drawable drawablePacote = resourceUtil.devolveDrawable(this, pacote.getImagem());
-        imagem.setImageDrawable(drawablePacote);
+    }
 
-        TextView data = findViewById(R.id.resumo_compra_data_pacote);
-        String periodoEmTexto = dataUtil.periodoEmTexto(pacote.getDias());
-        data.setText(periodoEmTexto);
-
+    private void mostraPreco(Pacote pacote) {
         TextView preco = findViewById(R.id.resumo_compra_preco_pacote);
         String moedaFormatada = moedaUtil.formataMoedaBrasileira(pacote.getPreco());
         preco.setText(moedaFormatada);
-        
+    }
+
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_compra_data_pacote);
+        String periodoEmTexto = dataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(periodoEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawablePacote = resourceUtil.devolveDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawablePacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_compra_local_pacote);
+        local.setText(pacote.getLocal());
     }
 }
